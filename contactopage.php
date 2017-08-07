b4e9c7f09d9bb67bfa8cb63cfd59aa76f9452812
Jorge Robalino <jrobalino@like.com.ecm>
2017-08-07 17:46:27 -0500

    antes de subida

--- contactopage.php
+++ contactopage.php
@@ -10,7 +10,8 @@ Template name: contacto
 	<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d16625.39923855429!2d-79.92408203557915!3d-2.1392811981427946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d6d7cd517422f%3A0xaa4d6e2a211bc035!2sDo%C3%B1a+Petrona!5e0!3m2!1ses!2ses!4v1501190901017" width="100%" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
  
-<div class="container" style="margin-top: 60px; margin-bottom: 60px;">
+<div class="container" style="margin-top: 60px; margin-bottom: 60px;" >
+<!--
 <h1 class="col-xs-12" style="font-size: 24px; font-weight: 800;">ENVÍENOS SUS COMENTARIOS</h1>
 	<div class="col-md-9 col-xs-12">
 		<form action="http://www.dpetrona.com/form/form1.php" class="contactanos" method="post">
@@ -21,7 +22,7 @@ Template name: contacto
 			<button type="submit" class="cta-enviar col-xs-12 col-md-6 col-md-offset-3">Enviar</button>
 		</form>
 	</div>
-
+ 
 	<div class="col-md-3 col-xs-12 abajo" style="margin-top: 15px;" >
 		<div class="col-xs-12">	
 			<span><i class="fa fa-map-marker" aria-hidden="true"></i>
@@ -37,5 +38,5 @@ Av. Juan Tanca Marengo km. 4.5 diagonal a la planta de Coca Cola. <br></div>
 		</div>
 	</div>
 </div>
-
+-->
 <?php include "footer.php"; ?>
